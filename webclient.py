bdbdb75bbc5ef02ba82d7a6135d645e749fed7af
Chris Morgan <cmorgan@abamaxa.com>
2018-02-15 09:08:27 +0000

    Webclient now renames downloaded image/json files, allowing it to specifiy the name of the first images so it no longer needs to be configured on the server.

--- webclient.py
+++ webclient.py
@@ -14,11 +14,13 @@ import time
 import argparse
 
 class DaemonClient :
-    def __init__(self, image_server_url, save_to_directory) :
+    def __init__(self, image_server_url, save_to_directory, verbose = False) :
         self.image_server_url = image_server_url
         self.save_to_directory = save_to_directory
         self.images_downloaded = 0
         self.http_session = None
+        self.image_filename = 1
+        self.verbose = verbose
         
         os.makedirs(save_to_directory, exist_ok=True)
         
@@ -32,20 +34,29 @@ class DaemonClient :
         async with self.http_session.get(self.image_server_url) as response :
             if response.content_type == "application/zip" and response.status == 200 :
                 self.__save_image_zip(io.BytesIO(await response.read()), response.headers)
-            #else :
-                #print(await response.text())
-                #print('.', end='')
                  
     def __save_image_zip(self, zip_bytes_file_obj, headers) :   
         m = re.search('filename="(\d+\.zip)"', headers.get('Content-Disposition'))
         if m :
-            filename = m.group(1)
-            print("Received file:", filename)
-            
+            zip_filename = m.group(1)
+        else :
+            raise Exception("Suspicious zip file - did not receive a filename in the header")
+
         with zipfile.ZipFile(zip_bytes_file_obj, 'r') as image_zip:
-            image_zip.extractall(self.save_to_directory)
-            
+            for zip_info in image_zip.infolist() :
+                stem, extension = os.path.splitext(zip_info.filename)
+                save_as_filename = os.path.join(
+                    self.save_to_directory, 
+                    "{}{}".format(str(self.image_filename), extension))
+                
+                image_zip.extract(zip_info, path=save_as_filename)
+                if self.verbose :
+                    print("Extracting {} to {}".format(zip_filename, save_as_filename))
+        
+        self.image_filename += 1
         self.images_downloaded += 1
+        if self.images_downloaded % 100 == 0 :
+            print("Downloaded {} images".format(self.images_downloaded))
             
     def __download_image_sync(self) :
         response = requests.get(self.image_server_url)
@@ -55,16 +66,19 @@ class DaemonClient :
         else :
             print(response.text)
             
-    def download_images(self, number_of_images_to_get) : 
+    def download_images(self, first_image_filename, number_of_images_to_get) : 
         try :
+            self.image_filename = first_image_filename
             start_time = time.time()
             self.images_downloaded = 0
             loop = asyncio.get_event_loop()
          
             while self.images_downloaded  < number_of_images_to_get :
                 loop.run_until_complete(self.__download_image())
-                
+              
+            loop.run_until_complete(self.http_session.close())  
             loop.close()   
+                       
             print("Downloaded {} images in {:.2f} seconds".format(
                 number_of_images_to_get, time.time() - start_time))
         except aiohttp.client_exceptions.ClientConnectionError as connect_error :
@@ -74,9 +88,14 @@ class DaemonClient :
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("count", type=int, help="Number of images to download")
+    parser.add_argument("-v", "--verbose", action="store_true", 
+                        help="Produce verbose output on progress")    
+    parser.add_argument("-s", "--start", type=int, default=1, 
+                        help="Integer filename of first image (images are names in a sequence 1.png, 2.png etc)")
+    parser.add_argument("-c", "--count", type=int, default=1, 
+                        help="Number of images to download")
     parser.add_argument("url",  help="url to download images from")
     
     args = parser.parse_args()    
-    client = DaemonClient(args.url, "www_output")
-    client.download_images(args.count)
+    client = DaemonClient(args.url, "www_output", args.verbose)
+    client.download_images(args.start, args.count)
