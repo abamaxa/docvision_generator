eed7cb303371436f4ab1ab249945a15dba214f99
Chris Morgan <cmorgan@abamaxa.com>
2018-05-20 10:57:54 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,16 +1,98 @@
-# doc_vision_synth
+# Docvision Synth
 
+## Introduction
+
+This is a tool for generating synthetic text documents for training object detection neural networks. It provides a command line and web interface. The tool can also export the data in YOLO or TFRecord format (for use with Tensorflow).
+
+## Installation
+
+Dependancies can be installed by running the following command.
+
+```shell
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+The style and layout of the generated documents are specified in a set of JSON configuration files. A top-level page configuration controls page level parameters such as margins, paddings and fonts. At a lower level, individual blocks of text or graphics (referred to as fragments) are specified in separate JSON files. Each fragment maps to a class of object for use when training an object detection net.
+
+For further details and examples see the page templates and page fragments directories.
+
+The tool can be run from the command line or as web server (see below).
+
+For instance, the following command produces 20 images, 1000 x 600 in size using the specified template file:
+
+```shell
+$ python main.py -d 1000 -w 600 -l 1000 --template=template.json 20
+```
+
+See the built in help for more options.
+
+## Docker Support
+
+A docker image can be built for running the webserver using the provided Dockerfile:
+
+```shell
+$ docker build -t doc_vision_synth .
+```
+
+The following commands tests the docker build by downloading 20 images:
+
+```shell
+$ docker run -p 4000:80 doc_vision_synth --daemon -d 600 -l 600 -w 600 25
+$ python webclient.py --url=http://localhost:4000/page --count=20
+```
+
+You can uploaded the docker image to your account on docker's public repository:
+
+```shell
+$ docker tag doc_vision_synth your_account_name/doc_vision_synth:v2.0
+$ docker push your_account_name/doc_vision_synth:v2.0
+```
+The webclient supports starting and stopping a Google cloud compute instance running the docker image.
+
+For instance, the following command will start a preconfigured Google compute cloud instance, download 20000 images and then stop the instance:
+
+```shell
+$ python webclient.py --zone=zone_hosting_your_instance \ --project=your_google_project \
+--instance=your_instance_name \
+--count=20000
+```
+
+##Export
+
+Images can be exported for use with Tensorflow by using the following command (note this script requires the Tensorflow [object_detection](https://github.com/tensorflow/models/tree/master/research/object_detection) and [slim](https://github.com/tensorflow/models/tree/master/research/slim) modules to be on the python path
+
+```shell
+$ python export/main.py --image_dir=directory_containing_images \
+--output_dir=directory_to_records_to \
+--name=a_name_for_the_dataset \
+tensorflow
+```
+
+and the following to export in YOLO format:
+
+```shell
+$ python export/main.py --image_dir=directory_containing_images \
+--output_dir=directory_to_records_to \
+--name=a_name_for_the_dataset \
+yolo
+```
 
 ##Tests
 
-python -m unittest discover -p "*_test.py"
+A comprehensive set of tests can be run using the following command.
 
-## Docker
+```shell
+$ python -m unittest discover -p "*_test.py"
+```
 
-docker build -t doc_vision_synth .
+##License
 
-docker run -p 4000:80 doc_vision_synth --daemon -d 600 -l 600 -w 600 25
+This software is released under the MIT licence.
 
-docker tag doc_vision_synth chrismorgan64/doc_vision_synth:v2.0
+## TODO
 
-docker push chrismorgan64/doc_vision_synth:v2.0
+- Export to PASCAL VOC format
+- Further develop web interface as the main method for using the tool.
+- Then provide a manual.
--- graphics/bounds.py
+++ graphics/bounds.py
@@ -2,31 +2,31 @@ from abc import abstractmethod
 
 class BoundsError(Exception):
     pass
-        
+
 class DimensionObject(object) :
     def __init__(self) :
         self._counter = 0
-        
+
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.__dict__ == other.__dict__
         else:
-            return False    
-        
+            return False
+
     def __iter__(self):
         self._counter = 0
-        return self    
-    
+        return self
+
     def __next__(self):
-        return self.next()   
-    
+        return self.next()
+
     def next(self):
         try :
             value = self.__getitem__(self._counter)
             self._counter += 1
             return value
         except IndexError :
-            raise StopIteration     
+            raise StopIteration
 
 
 class Size(DimensionObject) :
@@ -34,73 +34,73 @@ class Size(DimensionObject) :
         super().__init__()
         if not width is None and width < 0 :
             raise BoundsError("Width cannot be less than zero : '{}'".format(width))
-        
+
         if not height is None and height < 0 :
             raise BoundsError("Height cannot be less than zero : '{}'".format(height))
-        
+
         self._width = width
         self._height = height
-        
+
     @property
     def width(self) :
         return int(self._width)
-    
+
     @property
     def height(self) :
         return int(self._height)
-    
+
     def __repr__(self) :
         return "Width: {} Height: {}".format(self.width, self.height)
-    
+
     def __getitem__(self, index):
         if index == 0 :
             return int(self._width)
         elif index == 1 :
-            return int(self._height)   
+            return int(self._height)
         else :
             raise IndexError
-    
+
     def __len__(self) :
         return 2
-   
-    
+
+
 class Origin(DimensionObject) :
     def __init__(self, x = None, y = None) :
-        super().__init__()            
+        super().__init__()
         self._x = x
         self._y = y
-        
+
     @property
     def x(self) :
         return int(self._x)
-    
+
     @property
     def y(self) :
         return int(self._y)
-    
+
     def __repr__(self) :
-        return "x: {} y: {}".format(self.x, self.y)    
-    
+        return "x: {} y: {}".format(self.x, self.y)
+
     def __getitem__(self, index):
         if index == 0 :
             return int(self._x)
         elif index == 1 :
-            return int(self._y)  
+            return int(self._y)
         else :
-            raise IndexError      
-    
+            raise IndexError
+
     def __len__(self) :
-        return 2    
-    
+        return 2
+
     def next(self):
         self._counter += 1
         if self._counter == 1 :
             return self._x
         elif self._counter == 2 :
             return self._y
-        raise StopIteration        
-           
-   
+        raise StopIteration
+
+
 class Bounds(DimensionObject) :
     def __init__(self, x = None, y = None, width = None, height = None, x2 = None, y2 = None) :
         super().__init__()
@@ -111,76 +111,76 @@ class Bounds(DimensionObject) :
             self._size = Size(x2 - x, y2 - y)
         else :
             self._size = Size(0, 0)
-            
+
     @property
     def size(self) :
         return self._size
-    
+
     @property
     def origin(self) :
-        return self._origin    
-    
+        return self._origin
+
     @property
     def x(self) :
         return self._origin.x
-    
+
     @property
     def y(self) :
-        return self._origin.y     
-    
+        return self._origin.y
+
     @property
     def x2(self) :
         return self.x + self.width
-    
+
     @property
     def y2(self) :
         return self.y + self.height
-    
+
     @property
     def width(self) :
         return self._size.width
-    
+
     @property
     def height(self) :
-        return self._size.height       
-    
+        return self._size.height
+
     @property
     def rectangle(self) :
         return ((self.x, self.y), (self.x2, self.y2))
-    
+
     @property
     def region(self) :
-        return (self.x, self.y, self.x2, self.y2)    
-    
+        return (self.x, self.y, self.x2, self.y2)
+
     def inflate(self, inflate_by_x, inflate_by_y):
         return Bounds(self.x - inflate_by_x,
                       self.y - inflate_by_y,
                       self.width + inflate_by_x + inflate_by_x,
                       self.height + inflate_by_y + inflate_by_y)
-    
+
     def scale(self, xscale, yscale):
         return Bounds(self.x * xscale,
                       self.y * yscale,
                       self.width * xscale,
                       self.height * yscale)
-    
-    def move_to(self, x, y):    
-        return Bounds(x, y, self.width, self.height)  
-    
-    def resize(self, width, height):    
-        return Bounds(self.x, self.y, width, height)      
-    
-    def move(self, xoffset, yoffset):    
+
+    def move_to(self, x, y):
+        return Bounds(x, y, self.width, self.height)
+
+    def resize(self, width, height):
+        return Bounds(self.x, self.y, width, height)
+
+    def move(self, xoffset, yoffset):
         return Bounds(self.x + xoffset,
                       self.y + yoffset,
-                      self.width, self.height)    
-    
+                      self.width, self.height)
+
     def merge(self, bounds) :
         return Bounds(min(self.x,  bounds.x ),
                       min(self.y,  bounds.y ),
                       x2 = max(self.x2, bounds.x2),
-                      y2 = max(self.y2, bounds.y2))       
-    
+                      y2 = max(self.y2, bounds.y2))
+
     def enclosed_by_bounds(self, bounds):
         return (
             (self.x  >= bounds.x) and
@@ -192,29 +192,29 @@ class Bounds(DimensionObject) :
             (self.y2 >= bounds.y) and
             (self.y2 <= bounds.y2)
         )
-    
+
     @staticmethod
     def __overlap(bounds, bounds2) :
         return  (
-            ((bounds.x  >= bounds2.x) and
+            (((bounds.x  >= bounds2.x) and
              (bounds.x  <= bounds2.x2)) or
             ((bounds.x2 >= bounds2.x) and
-             (bounds.x2 <= bounds2.x2)) or
-            ((bounds.y  >= bounds2.y) and
+             (bounds.x2 <= bounds2.x2))) and
+            (((bounds.y  >= bounds2.y) and
              (bounds.y  <= bounds2.y2)) or
             ((bounds.y2 >= bounds2.y) and
-             (bounds.y2 <= bounds2.y2))
+             (bounds.y2 <= bounds2.y2)))
         )
-    
+
     def overlap_bounds(self, bounds):
         return Bounds.__overlap(self, bounds) or Bounds.__overlap(bounds, self)
-    
+
     def __repr__(self) :
-        return "{} {}".format(str(self._origin), str(self._size))    
-    
+        return "{} {}".format(str(self._origin), str(self._size))
+
     def __len__(self) :
-        return 5    
-    
+        return 5
+
     def __getitem__(self, index):
         if index == 0 :
             return (int(self.x), int(self.y))
@@ -226,5 +226,5 @@ class Bounds(DimensionObject) :
             return (int(self.x), int(self.y2))
         elif index == 4 :
             return (int(self.x), int(self.y))
-        
-        raise IndexError       
+
+        raise IndexError
